e7de97427a1f264bbca3e5554c1e1270f3a6e50e
brunonelson <47126837+brunonelson@users.noreply.github.com>
2019-02-13 17:40:05 -0500

    Add files via upload

---
+++ self/self.js
@@ -0,0 +1,47 @@
+le+ img;
+
+
+
+function setup(){
+    var cnv = createCanvas(710,400);
+    img = loadImage('marry.jpg');
+    
+    
+    
+}
+
+function draw(){
+    background(119,199,199);
+    image(img,0,0,width, height);
+    translate(width/2, height/2);
+    fill(1,4,0);
+    ellipse(0,0,130,160);
+    fill(255,200,140);
+    ellipse(0,0,100,140);
+    fill(253,253,253,253,);
+    ellipse(-20,-10,20,10);
+    fill(253,253,253,253);
+    ellipse(20,-10,-20,-10);
+    fill(19,88,2);
+    ellipse(-20,-10,8,8);
+    fill(19,88,2);
+    ellipse(20,-10,-8,-8);
+    fill(0,0,0,0);
+    ellipse(0,39,18,0);
+    fill(1,4,0)
+    rect(-60,69,120,150);
+    fill(82,52,5);
+    ellipse(0,-50,85,44)
+    fill(0,0,0,0);
+    ellipse(0,18,0,10);
+    fill(0,0,0,0);
+    ellipse(6,22,9,0);
+    
+
+    
+    
+    
+    
+    
+    
+}
